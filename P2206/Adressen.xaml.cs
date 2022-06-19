c18dd1f9eac9d33ce2726ee3ea687224e67cc587
Roland Kruggel <roland.kruggel@turck.com>
2022-06-19 10:52:58 +0200

    TellId erstellen

--- P2206/Adressen.xaml.cs
+++ P2206/Adressen.xaml.cs
@@ -28,9 +28,9 @@ namespace P2206
     public partial class Adressen : Page
     {
         private int a;
+        //DbAdressen dbAdressen;
 
-
-        DbAdressen dbAdressen = new()
+        DbAdressen dbAdressen = new() 
         {
             Id = new ObjectId(),                       //.Text = "878122";
             TellId = "RolandKruggel" + new Random().Next(100, 999).ToString(),    //.Text = "RolandKruggel824";
@@ -55,7 +55,7 @@ namespace P2206
 
 
 
-        public Adressen()
+    public Adressen()
         {
             InitializeComponent();
             this.DataContext = dbAdressen;
@@ -121,65 +121,112 @@ namespace P2206
         #endregion Litedb Text ----------------------------------------------------
 
 
+        #region Screen Adressen geladen
+
         private void grdMain_Loaded(object sender, RoutedEventArgs e)
         {
             //liteDbTest();       // aufruf des Test
 
 
 
-            var li = dbAdressen.EditorSonstigesR.Split('\n');
-            for (int i = 0; i < li.Length; i++)
-            {
-                var item = li[i];
-                var asu = "(?alter?)";
-                if (item.Contains(asu)) {
-                    var ld = item.Split('\t', StringSplitOptions.RemoveEmptyEntries);
-                    var dat = ld[0].Replace(asu, "").Trim();
-                    var parseDate = DateTime.Parse(dat);
+            //var li = dbAdressen.EditorSonstigesR.Split('\n');
+            //for (int i = 0; i < li.Length; i++)
+            //{
+            //    var item = li[i];
+            //    var asu = "(?alter?)";
+            //    if (item.Contains(asu)) {
+            //        var ld = item.Split('\t', StringSplitOptions.RemoveEmptyEntries);
+            //        var dat = ld[0].Replace(asu, "").Trim();
+            //        var parseDate = DateTime.Parse(dat);
 
-                    int years = DateTime.Now.Year - parseDate.Year;
-                    parseDate = parseDate.AddYears(years);
-                    if (DateTime.Now.CompareTo(parseDate) < 0) { years--; }
+            //        int years = DateTime.Now.Year - parseDate.Year;
+            //        parseDate = parseDate.AddYears(years);
+            //        if (DateTime.Now.CompareTo(parseDate) < 0) { years--; }
 
-                    var newDat = $"{years} Jahre";
-                    item.Replace(asu, newDat);
+            //        var newDat = $"{years} Jahre";
+            //        item.Replace(asu, newDat);
 
-                    a = 0;
-                }
-            }
+            //        a = 0;
+            //    }
+            //}
 
             // abhängig von der Combobox 'Alle Daten' werden die Daten angezeigt
             ckBox_Click(null, null);
 
-            //lvDataBinding.ItemsSource = sjj;
         }
+
+        #endregion
+
+
+        #region Button Del
 
         private void btnCmdDel_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
+        #endregion
+
+
+        #region Button Edit
+
         private void btnCmdEdit_Click(object sender, RoutedEventArgs e)
         {
             var ajj = Db.getAllAdressen();
         }
 
+        #endregion
+
+
+        #region Button Add
+
         private void btnCmdAdd_Click(object sender, RoutedEventArgs e)
         {
-            dbAdressen.Id = new ObjectId();
-            dbAdressen.Version = 3;
-            dbAdressen.Deaktiv = true;
-            dbAdressen.Anlage = DateTime.Now;
-            dbAdressen.Geaendert = DateTime.Now;
+            dbAdressen = new DbAdressen()
+            {
+                Id = new ObjectId(),                       //.Text = "878122";
+                TellId = "",    //.Text = "RolandKruggel824";
+                TellFlag = "y",
+                Version = 0,                    //Die Version. Wird bei jedem ändern un eins erhöht; default = 0
+                CmbTyp = 0,                     //.SelectedIndex = 0;
+                Deaktiv = false,
+                Anlage = DateTime.Now,          //.Text = DateTime.Now.ToString("g");
+                Geaendert = DateTime.Now,       //.Text = DateTime.Now.ToString("g");           
+                EditorNameL = "Anrede:\nTitel:\nName:\nZusatz:",    //.Text = "Anrede:\tHerr \nTitel:\tProf. Dr. vet. \nName:\tRoland Kruggel";
+                EditorNameR = "",
+                EditorAnschriftL = "Straße:\nOrt:\nLand:",       //.Text = "Straße:\tBeverst. 21\nOrt:\t58553 Halver\nLand:\tGermany";
+                EditorAnschriftR = "",
+                EditorKontaktL = "Tel:\nHandy:\nArbeit:\n",         //.Text = "Tel:\t02353 6699940\nHandy:\t0174 2170044\nArbeit:\t02353 709-6271\neMail:\trkruggel@bbf7.de\neMail:\trkruggel@gmx.de\nwww\t\twww.bbf7.de\n";
+                EditorKontaktR = "",
+                EditorInternetL = "eMail:\nwww:",
+                EditorInternetR = "",
+                EditorSonstigesL = "geschl.:\ngeb.:",        //.Text = "geschl.:\tmännlich\ngeb.:\t\t2.1.1959 (?alter?)";
+                EditorSonstigesR = "",
+                EditorBem = "--"
+            };
+
 
             var ahh = dbAdressen.CmbTyp;
-            
+
+            this.DataContext = null;
+            this.DataContext = dbAdressen;
+
+            ckBox_Click(null, null);
         }
 
+        #endregion
+
+
+        #region Button Ok
         private void btnCmdOk_Click(object sender, RoutedEventArgs e)
         {
             Db.saveAdresse(dbAdressen);
         }
+
+        #endregion
+
+
+        #region ListView
 
         /// <summary>
         /// Event
@@ -198,7 +245,13 @@ namespace P2206
            }
         }
 
-
+        /// <summary>
+        /// Event
+        /// Wenn ein Eintrag in der ListView geklickt wird.
+        /// Um zu selectieren muss zwei mal geklickt werden.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ckBox_Click(object sender, RoutedEventArgs e)
         {
             List<DbAdressen> listOfDbadr;
@@ -214,5 +267,76 @@ namespace P2206
             }
             lvDataBinding.ItemsSource = listOfDbadr;
         }
+
+        #endregion
+
+
+        #region txtName 
+
+        /// <summary>
+        /// Event
+        /// Wird ausgelöst wenn in txtNameR eine Taste gedrückt wird
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtNameR_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            var tx = sender as TextBox;
+
+            if (e.Key == Key.Enter || e.Key == Key.Down || e.Key == Key.Up)
+            {
+                DbAdressen? vm = DataContext as DbAdressen;
+                if(vm.TellId.Trim().Length == 0)
+                    vm.TellId = createTellId();
+            }
+            a = 0;
+        }
+
+        /// <summary>
+        /// Erzeugt eine neue TellId
+        /// Die TellId darf nicht geändert werden. Sie ist die Haupt-Such-ID
+        /// </summary>
+        /// <returns></returns>
+        private string createTellId()
+        {
+            var newText = "";
+
+            // Die linke und rechte Textbox als Liste 
+            List<string> textboxL = ((TextBox)FindName($"txtNameL")).Text.Split('\n').ToList();
+            List<string> textboxR = ((TextBox)FindName($"txtNameR")).Text.Split('\n').ToList();
+
+            // Der Index von Vorname und Name
+            var indexV = textboxL.FindIndex(x => x.StartsWith("Vorn"));
+            var indexN = textboxL.FindIndex(x => x.StartsWith("Name"));
+
+            try
+            {
+                if (indexV != -1)
+                {
+                    newText = textboxR[indexV];
+                }
+
+                if (indexN != -1)
+                {
+                    newText += textboxR[indexN];
+                }
+
+                newText = newText.Replace(" ", "").Replace("\t", "").Replace("\n", "");
+
+                if (newText.Length == 0)
+                    return "";
+
+                newText += new Random().Next(100, 999).ToString();
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+
+            return newText;
+        }
+
+        #endregion
+
     }
 }
